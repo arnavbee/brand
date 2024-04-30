8bfba3d5efde5885798cc304164b6afa1e465219
Arnav <105158586+arnavbee@users.noreply.github.com>
2024-04-30 11:53:15 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,30 @@
+function Hero(){
+
+return (<div className="hero container">
+    <div className="hero-content">
+    <h1>YOUR FEET DESERVE THE BEST</h1>
+    <h4>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
+        <br />YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</h4>
+    <button className="shop-now">Shop Now</button>
+    <button className="category">Category</button>
+   
+
+    <div className="shopping">
+        <p>Also Available On:</p>
+    <div className="icons">
+        <img src="./images/amazon.png" alt="" />
+        <img src="./images/flipkart.png" alt="" />
+    </div>
+    </div>
+    </div>
+            
+            <div className="hero-image"><img src="./images/shoe_image.png" alt="" /></div>
+    
+    </div>)
+
+        
+
+
+}
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+function Navbar(){
+    return ( <div className="logo">
+    <img src="/images/brand_logo.png" alt="" />
+
+
+
+<ul>
+<li>MENU</li>
+<li>ABOUT</li>
+<li>CONTACT</li>
+<li>CAREER</li>
+</ul>
+
+<button>Login</button>
+     
+   
+    </div>)
+
+}
+
+export default Navbar;
